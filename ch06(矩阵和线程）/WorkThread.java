d88d8696b2985c32c8bf114642879597e444834f
zhaoreer <43236261+zhaoreer@users.noreply.github.com>
2018-11-12 22:27:24 +0800

    Add files via upload

---
+++ ch06(矩阵和线程）/WorkThread.java
@@ -0,0 +1,38 @@
+package Thread;
+/**
+ * 
+ * @author 赵惹儿
+ *
+ * @version创建时间：2018年11月12日下午6:45:31
+ * @description: 工作线程类workThread
+ */
+
+public class WorkThread implements Runnable {
+	private int start;  //计算开始位置，以此区分工作线程工作任务
+	private int [][]A;
+	private int [][]B;
+	private int [][]C;
+	
+	//工作线程构造方法
+	public WorkThread(int start,int [][]A,int [][]B,int [][]C){
+		this.start=start;
+		this.A=A;
+		this.B=B;
+		this.C=C;
+	}
+	
+	@Override
+	public void run() {
+		int i,j,k;
+		//根据线程数量划分每个工作线程任务
+		for(i=start; i<Driver.M; i +=Driver.NUM_THREADS)      
+		{      
+			for(j=0;j<Driver.N;j++)      
+			{           
+				for( k=0; k< Driver.K;k++)         
+					C[i][j]+=A[i][k]*B[k][j];         
+			}
+		}
+	}
+ 
+}
